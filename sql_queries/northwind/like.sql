373b42cf0dffbc211fc5a25ad08e4e3e0fe619d4
iteegi <61802277+iteegi@users.noreply.github.com>
2023-01-17 17:42:04 +0300

    create simple queries with like

---
+++ sql_queries/northwind/like.sql
@@ -0,0 +1,12 @@
+select last_name, first_name 
+from employees e 
+where first_name like '%n';
+
+select last_name, first_name 
+from employees e 
+where last_name like 'B%';
+
+
+select last_name, first_name 
+from employees e 
+where last_name like '_uch%';
