a694d1952fb2e026a6057698a2f0eaea0be44cb9
iteegi <61802277+iteegi@users.noreply.github.com>
2023-01-17 16:40:34 +0300

    create basic selects

---
+++ sql_queries/northwind/basic.sql
@@ -0,0 +1,23 @@
+select count(*)
+from products
+
+select product_name, unit_price * units_in_stock 
+from products
+
+select distinct city 
+from employees e 
+
+select distinct country, city  
+from employees e 
+
+select count(distinct country) 
+from employees e 
+
+select count(*) region 
+from employees e 
+
+
+
+
+
+
