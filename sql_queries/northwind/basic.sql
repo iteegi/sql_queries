0ec451e19b8d35fe0b7f24032267f681000374b5
iteegi <61802277+iteegi@users.noreply.github.com>
2023-01-17 16:51:50 +0300

    add ;

--- sql_queries/northwind/basic.sql
+++ sql_queries/northwind/basic.sql
@@ -1,20 +1,20 @@
 select count(*)
-from products
+from products;
 
 select product_name, unit_price * units_in_stock 
-from products
+from products;
 
 select distinct city 
-from employees e 
+from employees e ;
 
 select distinct country, city  
-from employees e 
+from employees e ;
 
 select count(distinct country) 
-from employees e 
+from employees e ;
 
 select count(*) region 
-from employees e 
+from employees e ;
 
 
 
